3eafa47dfb9ce60447e17109ebeea8e3c382b8a1
Jeremiah Zuraff <jzuraff@metabank.com>
2018-12-03 08:24:29 -0600

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,6 +14,30 @@ namespace GradeBook.GradeBooks
         }
 
         /// <inheritdoc />
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        /// <inheritdoc />
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
+
+        /// <inheritdoc />
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
